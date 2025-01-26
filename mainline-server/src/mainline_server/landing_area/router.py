06a838137de0ad90e3d30e692d57f66db772f253
Mike Cowie <mikecowie@fastmail.com>
2025-01-26 23:37:43 +1300

    feat: basic content negotiation for the home page

--- mainline-server/src/mainline_server/landing_area/router.py
+++ mainline-server/src/mainline_server/landing_area/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 
 from mainline_server.rest_schema import APIResource, BaseResource
-from mainline_server.rest_utils import provide_hyperlink
+from mainline_server.rest_utils import negotiated, provide_hyperlink
 
 router = APIRouter()
 
@@ -12,9 +12,13 @@ class HomePage(BaseResource):
 
 @router.get("/", response_model=APIResource[HomePage])
 def api_root(request: Request):
-    return APIResource(
-        self=provide_hyperlink(request, "Home", "api_root"),
-        title="Home",
-        object={},
-        links=[],
+    """The home page, initial landing spot"""
+    return negotiated(
+        request,
+        APIResource(
+            self=provide_hyperlink(request, "Home", "api_root"),
+            title="Home",
+            object={},
+            links=[],
+        ),
     )
--- mainline-server/src/mainline_server/rest_utils.py
+++ mainline-server/src/mainline_server/rest_utils.py
@@ -2,8 +2,9 @@ from enum import StrEnum
 from typing import Mapping
 
 from fastapi import HTTPException, Request, status
+from fastapi.responses import HTMLResponse
 
-from mainline_server.rest_schema import APILink
+from mainline_server.rest_schema import APILink, BaseResource
 
 
 class ContentType(StrEnum):
@@ -50,3 +51,28 @@ def provide_hyperlink(
 ) -> APILink:
     """Provides APILink object to help add hypermedia to response objects"""
     return APILink(href=str(request.url_for(target, **path_params)), name=name)  # type: ignore
+
+
+def negotiated(request: Request, object: BaseResource):
+    """Returns a negotiated response object"""
+    preferred = allowable_content_types(request.headers)[0]
+    if preferred == ContentType.JSON:
+        # FastAPI itself will render the pydantic object
+        # Into a response object
+        return object
+    if preferred == ContentType.HTML:
+        # render via the whole HTML rendering machinery
+        # which is a whole other module to be designed still
+        return HTMLResponse(
+            content=(
+                "<html><body><a>HTML response was negotiated, "
+                "but a proper HTML renderer has not been implemented yet. "
+                "Following is the JSON data that will be rendered</a>"
+                f"</br><code>{object.model_dump_json()}</code></body></html>"
+            )
+        )
+    raise HTTPException(
+        status_code=status.HTTP_406_NOT_ACCEPTABLE,
+        detail="caannot find acceptable response type for accept header "
+        "{request.headers.get('accept')}",
+    )
